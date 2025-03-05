18a904e1444ad52b08c40bc8ad8963d19f49e9e4
imarc <mojintroliso@gmail.com>
2025-03-05 23:47:32 +0800

    -first commit

---
+++ styles/auth.style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+
+    title:{
+      color:"red",
+      fontSize:25
+    },
+
+    button:{
+        backgroundColor:"green",
+        padding:16
+    }
+})
